13ad6655047c62614f6ab6bbd91b26f2cc4c3938
nupur-bhatt <42573031+nupur-bhatt@users.noreply.github.com>
2020-10-07 18:12:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    out = helpers.RandomNumberGenerator(0, 30)
-    result = out.generate_sequence(15)
+    out = helpers.RandomNumberGenerator(0, 100)
+    result = out.generate_sequence(30)
     output = tuple(result)
     response = app.response_class(response=json.dumps(output), status=200, mimetype='application/json')
     return response
