7a0351b3ff8c7c700f9fdb8a5fc0d928ee3020ed
Nupur Bhatt <Nupur.Bhatt@dal.ca>
2020-10-07 19:41:21 -0300

    Parameters added as variables in random number generator

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from flask import json
 
 app = Flask(__name__)
 
+# Enter parameters to generate a list of random numbers
+
+min_value=0;
+max_value=100;
+length=30;
 
 @app.route('/')
 def hello():
-    out = helpers.RandomNumberGenerator(0, 100)
-    result = out.generate_sequence(30)
+    out = helpers.RandomNumberGenerator(min_value, max_value)
+    result = out.generate_sequence(length)
     output = tuple(result)
     response = app.response_class(response=json.dumps(output), status=200, mimetype='application/json')
     return response
